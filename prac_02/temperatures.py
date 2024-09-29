2db6e29a1e3e03b42ba250fe54a96c846bf6a56a
MoeKhant7711 <moekhant.zaw@my.jcu.edu.au>
2024-09-29 18:40:04 +0800

    Create temperatures.py

---
+++ prac_02/temperatures.py
@@ -0,0 +1,20 @@
+def main():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    print(f"{celsius}°C is equal to {fahrenheit}°F")
+
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit}°F is equal to {celsius}°C")
+
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+main()
+
