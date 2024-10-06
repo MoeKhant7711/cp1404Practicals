fe95b433de51df437f4adf41d4adea2adcf463b1
MoeKhant7711 <moekhant.zaw@my.jcu.edu.au>
2024-10-06 18:50:40 +0800

    Create capitalist_conrad.py

---
+++ prac_03/prac_03/capitalist_conrad.py
@@ -0,0 +1,32 @@
+import random
+
+MAX_INCREASE = 0.175
+MAX_DECREASE = 0.05
+MIN_PRICE = 1.00
+MAX_PRICE = 100.0
+INITIAL_PRICE = 10.0
+FILENAME = "output.txt"
+
+out_file = open(FILENAME, 'w')
+
+price = INITIAL_PRICE
+number_of_days = 0
+
+print(f"Starting price: ${price:,.2f}", file=out_file)
+
+while MIN_PRICE <= price <= MAX_PRICE:
+    number_of_days += 1
+    price_change = 0
+
+    if random.randint(1, 2) == 1:
+        price_change = random.uniform(0, MAX_INCREASE)
+    else:
+        price_change = random.uniform(-MAX_DECREASE, 0)
+
+    price *= (1 + price_change)
+
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
+
+print(f"Simulation completed and written to {FILENAME}")
